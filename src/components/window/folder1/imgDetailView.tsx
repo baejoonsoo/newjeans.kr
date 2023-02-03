7024d442eca5ec06788f94e64c42f3606c6b7b85
BaeJoonSoo <baejoonsoo21@gmail.com>
2023-02-04 04:34:41 +0900

    img view

--- src/components/window/folder1/imgDetailView.tsx
+++ src/components/window/folder1/imgDetailView.tsx
@@ -1,6 +1,8 @@
+import { folder1ImgData } from '@/docs/folder1Img';
 import { folder1ImgDataType } from '@/interface/folder1ImgData';
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
+import { useState } from 'react';
 
 interface props {
   itemData: folder1ImgDataType;
@@ -11,24 +13,67 @@ const ImgDetailView: NextPage<props> = ({
   itemData,
   closeDetailView,
 }: props) => {
+  const [item, setItem] = useState<folder1ImgDataType>(itemData);
+
+  const prevImg = () => {
+    if (item.key !== 0) {
+      setItem(folder1ImgData[item.key - 1]);
+    }
+  };
+
+  const NextImg = () => {
+    if (item.key !== folder1ImgData.length - 1) {
+      setItem(folder1ImgData[item.key + 1]);
+    }
+  };
+
   return (
     <ImgDetailViewContainer>
       <WindowTitleWrap>
-        <WindowTitle>New Folder 1 - {itemData.name}</WindowTitle>
+        <WindowTitle>New Folder 1 - {item.name}</WindowTitle>
         <CLoseButton onClick={closeDetailView} />
       </WindowTitleWrap>
-      <ImgView img={itemData.url} />
+      <ImgView img={item.url}>
+        {item.key !== 0 ? <PrevButton onClick={prevImg} /> : <></>}
+        {item.key !== folder1ImgData.length - 1 ? (
+          <NextButton onClick={NextImg} />
+        ) : (
+          <></>
+        )}
+      </ImgView>
     </ImgDetailViewContainer>
   );
 };
 
+const NextButton = styled.button`
+  height: 44px;
+  aspect-ratio: 1;
+
+  background-image: url('/image/icon/nextArrow.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+`;
+
+const PrevButton = styled(NextButton)`
+  transform: rotate(180deg) translateY(50%);
+  left: 10px;
+`;
+
 const ImgView = styled.div`
   width: 100%;
   height: 772px;
+  padding: 10px;
   background-image: url(${({ img }: { img: string }) => img});
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
+  position: relative;
 `;
 
 const CLoseButton = styled.button`
