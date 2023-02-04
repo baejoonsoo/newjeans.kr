0576b76df59bec5ac0a0c92240d8ec006d26b8d3
BaeJoonSoo <baejoonsoo21@gmail.com>
2023-02-04 17:38:44 +0900

    video iframe

--- src/components/window/folder2/videoDetailView.tsx
+++ src/components/window/folder2/videoDetailView.tsx
@@ -1,40 +1,40 @@
-import { folder2ItemData } from '@/docs/folder2Item';
 import { folder2ItemDataType } from '@/interface/folder2ItemData';
 import { windowZIndexRecoil } from '@/utiles/store/windowZIndex';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import VideoDetailView from './videoDetailView';
 
 interface props {
-  closeWindow: () => void;
+  itemData: folder2ItemDataType;
+  closeDetailView: () => void;
 }
 
-const Folder2Window: NextPage<props> = ({ closeWindow }: props) => {
-  const windowRef = useRef<HTMLDivElement>(null);
+const youtubeURL = 'https://www.youtube.com/embed/';
+
+const VideoDetailView: NextPage<props> = ({
+  itemData,
+  closeDetailView,
+}: props) => {
   const [point, setPoint] = useState<{ x: number; y: number }>({
-    x: 100,
-    y: 70,
-  });
-  const [isShowDetailView, setIsShowDetailView] = useState<boolean>(false);
-  const [itemData, setItemData] = useState<folder2ItemDataType>({
-    key: 0,
-    name: '',
-    embedId: '',
+    x: 125,
+    y: 80,
   });
   const [zIndexRecoil, setZindexRecoil] = useRecoilState(windowZIndexRecoil);
   const [zIndex, setZindex] = useState<number>(zIndexRecoil);
+  const windowRef = useRef<HTMLDivElement>(null);
 
   const changeZIndex = () => {
     if (zIndex === zIndexRecoil - 1) return null;
+
     setZindex(zIndexRecoil);
     setZindexRecoil((pre) => pre + 1);
   };
 
   const mouseDown = (event: any) => {
     changeZIndex();
+
     if (!windowRef || !windowRef.current) return null;
 
     let shiftX = event.clientX - windowRef.current.getBoundingClientRect().left;
@@ -68,86 +68,33 @@ const Folder2Window: NextPage<props> = ({ closeWindow }: props) => {
     });
   };
 
-  const changeItemData = (item: folder2ItemDataType) => {
-    setItemData(item);
-    setIsShowDetailView(true);
-  };
-
-  const closeDetailView = () => {
-    setIsShowDetailView(false);
-  };
-
-  useEffect(() => {
-    setZindexRecoil((pre) => pre + 1);
-  }, []);
-
   return (
-    <>
-      <WindowSection ref={windowRef} x={point.x} y={point.y} zIndex={zIndex}>
-        <WindowTitleWrap onMouseDown={mouseDown}>
-          <WindowTitle>New Folder 2</WindowTitle>
-          <CLoseButton onClick={closeWindow} />
-        </WindowTitleWrap>
-        <ItemsContainer>
-          {folder2ItemData.map((videoItem) => (
-            <Item
-              key={videoItem.key}
-              onDoubleClick={() => changeItemData(videoItem)}
-            >
-              <ItemImg />
-              <ImgName>{videoItem.name}</ImgName>
-            </Item>
-          ))}
-        </ItemsContainer>
-      </WindowSection>
-      {isShowDetailView ? (
-        <VideoDetailView
-          itemData={itemData}
-          closeDetailView={closeDetailView}
-        />
-      ) : (
-        <></>
-      )}
-    </>
+    <VideoDetailViewContainer
+      ref={windowRef}
+      x={point.x}
+      y={point.y}
+      zIndex={zIndex}
+    >
+      <WindowTitleWrap onMouseDown={mouseDown}>
+        <WindowTitle>New Folder 1 - {itemData.name}</WindowTitle>
+        <CLoseButton onClick={closeDetailView} />
+      </WindowTitleWrap>
+      <VideoView>
+        <Embed src={`${youtubeURL + itemData.embedId}`} />
+      </VideoView>
+    </VideoDetailViewContainer>
   );
 };
 
-const ImgName = styled.p`
-  font-size: 12px;
-`;
-
-const ItemImg = styled.div`
-  width: 72px;
-  aspect-ratio: 1;
-
-  background-image: url('/image/icon/videoIcon.png');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 54px 40px;
-`;
-
-const Item = styled.div`
-  width: 95px;
-  height: 105px;
-
-  padding-top: 5px;
-  padding-bottom: 10px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-
-  cursor: pointer;
-`;
-
-const ItemsContainer = styled.div`
+const Embed = styled.iframe`
   width: 100%;
-  height: 500px;
-  padding: 5px;
+  aspect-ratio: 16/9;
+  border: none;
+`;
 
-  display: flex;
-  align-content: flex-start;
+const VideoView = styled.div`
+  width: 100%;
+  height: 360px;
 `;
 
 const CLoseButton = styled.button`
@@ -197,12 +144,14 @@ const WindowTitleWrap = styled.div`
   }
 `;
 
-const WindowSection = styled.section`
-  width: 500px;
-  height: 528px;
+const VideoDetailViewContainer = styled.section`
+  width: 640px;
+
   background-color: white;
 
   position: absolute;
+  top: 60px;
+  left: 275px;
 
   ${({ x, y, zIndex }: { x: number; y: number; zIndex: number }) => css`
     top: ${y}px;
@@ -211,4 +160,4 @@ const WindowSection = styled.section`
   `}
 `;
 
-export default Folder2Window;
+export default VideoDetailView;
